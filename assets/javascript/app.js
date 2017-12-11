e6e2a6ddd5b25893b28686e5c7cc47b72330e278
Gerardo Fernandez <gerardojose13@gmail.com>
2017-12-11 00:11:24 -0800

    pushes new updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,53 @@
+var config = {
+    apiKey: "AIzaSyANcUOX0qXiutqPqBMCNUEIQc30nrTppq8",
+    authDomain: "train-time-197d2.firebaseapp.com",
+    databaseURL: "https://train-time-197d2.firebaseio.com",
+    projectId: "train-time-197d2",
+    storageBucket: "",
+    messagingSenderId: "627419492606"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var train = "";
+var destination = "";
+var time = "";
+var frecuency = 0;
+
+$('#data-submit').on('click', function(){
+    event.preventDefault();
+
+    train = $('#train-name').val().trim();
+    destination = $('#destination').val().trim();
+    time = $('#time').val().trim();
+    frecuency = $('#frecuency').val().trim();
+
+    database.ref().push({
+        train_name: train,
+        destination: destination,
+        schedule: time,
+        frecuency: frecuency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
+});
+
+database.ref().on('child_added', function(snapshot){
+    var trainName = $('#trainName').addClass('striped');
+    var desti = $('#desti').addClass('striped');
+    var frec = $('#frec').addClass('striped');
+
+    trainName.append('<tr><td>' + snapshot.val().train_name + '</td></tr>');
+    desti.append('<tr><td>' + snapshot.val().destination + '</td></tr>');
+    frec.append('<tr><td>' + snapshot.val().frecuency + ' min' + '</td></tr>');
+})
+
+
+
+
+
+
 $('.timepicker').pickatime({
     default: 'now', // Set default time: 'now', '1:30AM', '16:30'
     fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
